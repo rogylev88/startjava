c03c5af9f834e9f056d63d90aadb30a9ece9319f
Leonid_R_88 <72568008+rogylev88@users.noreply.github.com>
2021-02-06 14:46:18 +0300

    implements HW_1

---
+++ lesson_01/Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+    public static void main(String[] args){
+        byte core = 4;
+        short ssd = 240;
+        int matherboard = 1;
+        long memory = 4;
+        float floppy = 6.02e23f;
+        double videocard = 2.5;
+        char modem = 1;
+        boolean btmodul = true;
+    System.out.println(core);
+    System.out.println(ssd);
+    System.out.println(matherboard);
+    System.out.println(memory);
+    System.out.println(floppy);
+    System.out.println(videocard);
+    System.out.println(modem);
+    System.out.println(btmodul);
+    }
+}
