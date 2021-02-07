af36bcbf4620fe1a0ff267a518b442c0df63ba87
Leonid_R_88 <72568008+rogylev88@users.noreply.github.com>
2021-02-07 17:46:16 +0300

    Update Variable.java

--- lesson_01/Variable.java
+++ lesson_01/Variable.java
@@ -2,12 +2,12 @@ public class Variable {
     public static void main(String[] args) {
         byte core = 4;
         short ssd = 240;
-        int matherboard = 1;
+        int motherBoard = 1;
         long memory = 4;
         float floppy = 6.02e23f;
-        double videocard = 2.5;
+        double videoCard = 2.5;
         char modem = 1;
-        boolean btmodul = true;
+        boolean btModul = true;
         System.out.println(core);
         System.out.println(ssd);
         System.out.println(motherBoard);
