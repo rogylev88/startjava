0c6c9b689d2c4adcc30b70cc5376511aae4db298
Leonid_R_88 <72568008+rogylev88@users.noreply.github.com>
2021-02-12 22:44:26 +0300

    Add files via upload

--- lesson_01/ConditionalStatement.java
+++ lesson_01/ConditionalStatement.java
@@ -2,30 +2,26 @@ public class ConditionalStatement {
     public static void main(String[] args) {
         int age = 22;
         if(age > 20) {
-            System.out.println("возраст больше 20");
-        }
+            System.out.println("возраст больше 20");}
+
         boolean male = true;
         if (male == true) {
             System.out.println("пол мужской");
-        }
-        else {
-            System.out.println("пол немужской");
-        }
+        }else {
+            System.out.println("пол немужской");}
+
         double height = 1;
         if(height < 1.80) {
             System.out.println("рост меньше 1.8м");
-        }
-        else {
-            System.out.println("рост больше 1.8м");
-        }
+        }else {
+            System.out.println("рост больше 1.8м");}
+
         char letterName = 'M';
         if(letterName == 'M') {
             System.out.println("Первая буква имени M");
-        }
-        else if(letterName == 'I') {
+        }else if(letterName == 'I') {
             System.out.println("Первая буква имени I");
-        }
-        else {
+        }else {
             System.out.println("иная буква");
         }
     }
