6a8281f8e8d5bbed9f640d577acdf08c4b5e21cf
Leonid_R_88 <72568008+rogylev88@users.noreply.github.com>
2021-02-14 16:27:47 +0300

    Add files via upload

---
+++ lesson_01/MyFirstGame.java
@@ -0,0 +1,17 @@
+public class MyFirstGame {
+    public static void main(String[] args) {
+        int a = 0; // Начальное значение диапазона - "от"
+        int b = 100; // Конечное значение диапазона - "до"
+        int randomNumber = a + (int) (Math.random() * b); // Задаем рандомность числу
+        int number = 50; // Переменная которой угадываем число
+        if(randomNumber > number) {
+            System.out.println("Введенное вами число больше того, что загадал компьютер");
+        }
+        else if(randomNumber < number) {
+            System.out.println("Введенное вами число меньше того, что загадал компьютер");
+        }
+        else  {
+            System.out.println("Вы угадали!");
+        }
+    }
+}
