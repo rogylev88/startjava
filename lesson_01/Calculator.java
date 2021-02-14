6ac9ab4179b37b2a33c2a977a76c275a06f67b93
Leonid_R_88 <72568008+rogylev88@users.noreply.github.com>
2021-02-14 15:10:32 +0300

    Add files via upload

--- lesson_01/Calculator.java
+++ lesson_01/Calculator.java
@@ -1,32 +1,31 @@
 public class Calculator {
     public static void main(String[] args) {
-        char a = '%';
-        int b = 10;
-        int c = 5;
-        int result = 1;
-        if(a == '+') {
-            result = b+c;
-            System.out.println(result);}
-
-        else if(a == '-') {
-            result = b-c;
-            System.out.println(result);}
-
-        else if(a == '*') {
-            result = b*c;
-            System.out.println(result);}
-
-        else if(a == '/') {
-            result = b/c;
-            System.out.println(result);}
-
-        else if(a == '^') {
-            result =(100 * b) / (c * c);
-            System.out.println(result);}
-
-
-        else if(a == '%') {
-            result = b%c;
+        char sign = '^';
+        double b = 10;
+        double c = 5;
+        double result = 1;
+        if(sign == '+') {
+            result = b + c;
+            System.out.println(result);
+        }
+        else if(sign == '-') {
+            result = b - c;
+            System.out.println(result);
+        }
+        else if(sign == '*') {
+            result = b * c;
+            System.out.println(result);
+        }
+        else if(sign == '/') {
+            result = b / c;
+            System.out.println(result);
+        }
+        else if(sign == '^') {
+            result =Math.pow(b, c);
+            System.out.println(result);
+        }
+        else if(sign == '%') {
+            result = b % c;
             System.out.println(result);}
         }
 }
--- lesson_01/Cycle.java
+++ lesson_01/Cycle.java
@@ -1,26 +1,23 @@
 public class Cycle {
     public static void main(String[] args) {
-        for(int i = 0; i<21;i++) {
-            System.out.println(i);}
-
+        for(int i = 0; i < 21; i++ ) {
+            System.out.println(i);
+        }
         int a = -6;
         while(a < 7) {
             System.out.println(a);
-            a += 2;}
-
+            a += 2;
+        }
         int i = 1;
-        int result = 9;
-        int n = 0;
+        int result = 10;
+        int sum = 0;
         do {
             result = result + i;
-            if(result % 2 == 0)
-            {
-                System.out.println("Число"+result+"является четным");
+            if(result % 2 == 0){
             } else {
-                n = n+result;
-                System.out.println("Число"+result+"является нечетным");
+                sum = sum + result;
             }
-            } while (result<20);
-            System.out.println("11+13+15+17+19= "+n);
+            } while (result < 20);
+            System.out.println("11+13+15+17+19= " + sum);
         }
     }
