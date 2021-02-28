ca2ae716a3ebe97cbf50c06fa0c544bea6cbe69c
Leonid_R_88 <72568008+rogylev88@users.noreply.github.com>
2021-02-28 17:36:42 +0300

    Add files via upload

--- lesson_01/MyFirstGame.java
+++ lesson_01/MyFirstGame.java
@@ -4,17 +4,23 @@ public class MyFirstGame {
     }
 
     public static int getNumber() {
-        int compNumber = 50; // Переменная компа
+        int compNumber = 10; // Переменная компа
         int playerNumber = 49; // Переменная которой угадываем число, его менять с каждым ходом
-        if (compNumber > playerNumber) {
-            System.out.println("Введенное вами число меньше того, что загадал компьютер");
-            return playerNumber++;
-        } else if (compNumber < playerNumber) {
-            System.out.println("Введенное вами число больше того, что загадал компьютер");
-            return playerNumber--;
-        } else {
-            System.out.println("Вы угадали!");
-            return playerNumber;
+            if (compNumber > playerNumber) {
+                while (playerNumber < compNumber) {
+                    System.out.println("Введенное вами число меньше того, что загадал компьютер");
+                    playerNumber ++;
+                    }
+                return playerNumber;
+            } else if (compNumber < playerNumber) {
+                while (playerNumber > compNumber) {
+                    System.out.println("Введенное вами число больше того, что загадал компьютер");
+                    playerNumber --;
+                    }
+                return playerNumber;
+            } else {
+                System.out.println("Вы угадали!");
+                return playerNumber;
         }
     }
 }
