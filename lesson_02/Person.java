6392cdc38c167c1d845c9ee606bd42dd4a130967
Leonid_R_88 <72568008+rogylev88@users.noreply.github.com>
2021-03-08 14:21:35 +0300

    Урок 2

---
+++ lesson_02/Person.java
@@ -0,0 +1,24 @@
+public class Person {
+
+    String name = "Human";
+    Boolean male = true;
+    int age = 22;
+    int height = 180;
+    int weight = 70;
+
+    void walk() {
+    }
+
+    void sit() {
+    }
+
+    void run() {
+    }
+
+    void talk() {
+    }
+
+    void learnJava() {
+    }
+
+}
